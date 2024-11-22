13e68aec903104d9de53027ee20df9e27cb6724f
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-11-22 15:17:04 +0100

    update: introduce fonts on layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Slot, Stack } from "expo-router";
-import globalCss from "../global.css";
+import { useFonts } from "expo-font";
 
 const RootLayout = () => {
+  const [fontLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-Poppins-ExtraLight": require("../assets/fonts/Poppins-Poppins-ExtraLight.ttf"),
+    "Poppins-Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "SpaceMono-Regular": require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
